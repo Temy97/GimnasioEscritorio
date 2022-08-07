13df43d5abbdef1da51fa977f9df4697981a8547
A. David Temiño Monedero González Gómez <agdrm97@gmail.com>
2022-08-07 12:06:01 +0200

    creada ventana del log
    Se ha creado la ventana del Log-In, falta dar funcionalidad a los botones y crear clases modelo para el acceso a neodatis.

--- src/vista_login/VistaLogIn.java
+++ src/vista_login/VistaLogIn.java
@@ -1,10 +1,99 @@
 package vista_login;
 
-public class VistaLogIn {
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
+public class VistaLogIn extends JFrame{
+	
+	//ATRIBUTOS:
+	private JButton botonAceptar;
+	private JButton botonSalir;
+	
+	
+	//CONSTRUCTOR:
+	public VistaLogIn() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setSize((int) (screenSize.getWidth() / 4), (int) (screenSize.getHeight() / 2));
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setTitle("Log-Menu");
+		
+		
+		//Panel de la ventna:
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		
+		JLabel nombre = new JLabel("Nombre:");
+		nombre.setBounds(80, 60, 80, 20);
+		JLabel pasword = new JLabel("Contraseña:");
+		pasword.setBounds(80, 140, 80, 20);
+		
+		JTextField txtNombre = new JTextField();
+		txtNombre.setBounds(200, 60, 120, 20);
+		JPasswordField txtPasword = new JPasswordField();
+		txtPasword.setEchoChar('*');
+		txtPasword.setBounds(200, 140, 120, 20);
+		
+		botonAceptar = new JButton("Aceptar");
+		botonAceptar.setBounds(60, 240, 100, 60);
+		botonAceptar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//comprobar escrito
+				if(txtNombre.getText().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Nombre erroneo", "", JOptionPane.ERROR_MESSAGE, null);
+				}else {
+					if(txtPasword.getPassword().length == 0) {
+						JOptionPane.showMessageDialog(null, "Contraseña erronea", "", JOptionPane.ERROR_MESSAGE, null);
+					}else {
+						
+					}
+				}
+				//comprobar db
+				//lanzar objeto
+			}
+		});
+		botonSalir = new JButton("Salir");
+		botonSalir.setBounds(200, 240, 100, 60);
+		botonSalir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		panel.add(nombre);
+		panel.add(pasword);
+		panel.add(txtNombre);
+		panel.add(txtPasword);
+		panel.add(botonAceptar);
+		panel.add(botonSalir);
+		
+		this.setContentPane(panel);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
-
+	
+	
+	//METODOS:
+	public static void main(String[] args) {
+		new VistaLogIn();
+	}
+	
+	
+	/*public static boolean vistaLogIn() {
+		
+	}*/
 }
